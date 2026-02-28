d0f260a33fd7dfc1b30abad08fa91ba14f964260
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-02-28 18:35:00 +0800

    feat: apply simple route definitions

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Short-hand route definition
+Route::view('/about', 'about');
+Route::view('/contact', 'contact');
