e2db9c9429d179f2b7fd83a0cdf0e3ef3edb6bed
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-03-02 18:13:24 +0800

    feat: set up dynamic title binding
    There are 2 modes: @props and attribute

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,4 @@
-<x-layout>
+<x-layout title="About Us">
     <h1>About ME</h1>
     <a href="/">Return Home</a>
 </x-layout>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,4 +1,4 @@
-<x-layout>
+<x-layout title="Contact">
     <h1>Contact Us</h1>
     <input type="number">
 </x-layout>
